bf6c7716317acca54793794e84c7b5a0eea3eaba
EstrellaJYKIM <puris9030@gmail.com>
2018-03-18 18:37:14 +0900

    testing routing todo page

---
+++ frontend/src/components/Todo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo">{{intro}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'Todo',
+    data () {
+      return {
+        intro: 'this is todo page with vuejs'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo{
+    color: orange;
+  }
+</style>
